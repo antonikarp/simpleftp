bcd04a2a3ceefd738eab02c02ba16964962125df
Antoni Karpinski <ant.karpinski@gmail.com>
2021-06-05 08:39:41 -0700

    Add global store.

--- server/thread-sv.c
+++ server/thread-sv.c
@@ -6,23 +6,23 @@ extern volatile sig_atomic_t do_work;
  * Each incoming request from a client is serviced by this function/
  */
 void* thread_worker(void *void_arg) {
-	struct thread_arg *arg = (struct thread_arg *) void_arg;
+	struct thread_arg *targ = (struct thread_arg *) void_arg;
 	char buf[BUFSIZE];
 	int cfd;
 	for (;;) {
-		while (!(*(arg->new_request_condition)) && do_work) {
-			if (pthread_cond_wait(arg->new_request_cond, arg->new_request_mutex) != 0) {
+		while (!(*(targ->ptr_new_request_condition)) && do_work) {
+			if (pthread_cond_wait(targ->ptr_new_request_cond, targ->ptr_new_request_mutex) != 0) {
 				ERR("pthread_cond_wait");
 			}
 		}
 		
-		*(arg->new_request_condition) = 0;
+		*(targ->ptr_new_request_condition) = 0;
 		if (!do_work) {
 			pthread_exit(NULL);
 		}
 		
-		cfd = *(arg->cur_client_fd);
-		if (pthread_mutex_unlock(arg->new_request_mutex) != 0) {
+		cfd = *(targ->ptr_cur_client_fd);
+		if (pthread_mutex_unlock(targ->ptr_new_request_mutex) != 0) {
 			ERR("pthread_mutex_unlock");
 		}
 		persist_read(cfd, buf, BUFSIZE);
@@ -34,7 +34,7 @@ void* thread_worker(void *void_arg) {
 /* init_threads
  * Intialize idle, detached threads.
  */
-void init_threads(pthread_t *threads, struct thread_arg *arg, struct thread_arg *args) {
+void init_threads(struct global_store *store) {
 	for (int i = 0; i < MAXCL; i++) {
 		pthread_attr_t thread_attr;
 		if (pthread_attr_init(&thread_attr)) {
@@ -43,12 +43,12 @@ void init_threads(pthread_t *threads, struct thread_arg *arg, struct thread_arg 
 		if (pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED)) {
 			ERR("pthread_attr_setdetachstate");
 		}
-		args[i].id = i;
-		args[i].cur_client_fd = arg->cur_client_fd;
-		args[i].new_request_condition = arg->new_request_condition;
-		args[i].new_request_cond = arg->new_request_cond;
-		args[i].new_request_mutex = arg->new_request_mutex;
-		if (pthread_create(&(threads[i]), &thread_attr, thread_worker, (void *) &args[i]) != 0) {
+		store->args[i].id = i;
+		store->args[i].ptr_cur_client_fd = &(store->cur_client_fd);
+		store->args[i].ptr_new_request_condition = &(store->new_request_condition);
+		store->args[i].ptr_new_request_cond = &(store->new_request_cond);
+		store->args[i].ptr_new_request_mutex = &(store->new_request_mutex);
+		if (pthread_create(&(store->threads[i]), &thread_attr, thread_worker, (void *) &(store->args[i])) != 0) {
 			ERR("pthread_create");
 		}
 		pthread_attr_destroy(&thread_attr);
